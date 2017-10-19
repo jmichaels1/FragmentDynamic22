3e08641aa00091eb83f009c3ee91056494609c75
iam4208017 <michael.salr1590@gmail.com>
2017-10-19 20:25:27 +0200

    Initial commit

---
+++ app/src/main/java/iam4208017/escoladeltreball/org/fragmentdynamic22/MainActivity.java
@@ -0,0 +1,13 @@
+package iam4208017.escoladeltreball.org.fragmentdynamic22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
